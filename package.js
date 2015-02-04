812fb33b55df544fd25a2b4ed1231c6e89d4f186
dolgarev <o.dolgarev@gmail.com>
2015-02-04 18:06:34 +0200

    bumped version number to 1.0.2 minor improvements

--- package.js
+++ package.js
@@ -6,13 +6,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2@1.1.2', ['client', 'server']);
-  api.use('oauth@1.1.3', ['client', 'server']);
-  api.use('http@1.0.10', ['server']);
-  api.use('underscore@1.0.2', 'client');
-  api.use('templating@1.0.11', 'client');
-  api.use('random@1.0.2', 'client');
-  api.use('service-configuration@1.0.3', ['client', 'server']);
+  api.versionsFrom('1.0.2');
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('underscore', 'client');
+  api.use('templating', 'client');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
 
   api.export('PaypalLogin');  
 
